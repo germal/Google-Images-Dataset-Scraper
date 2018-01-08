0da93ef36bc267cabc1f5129d81702484bbb6ab5
Andy <hinandy@gmail.com>
2018-01-08 18:16:49 -0500

    commit

---
+++ fetch.js
@@ -0,0 +1,17 @@
+// inject jquery
+var script = document.createElement('script');
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(script);
+
+// grab image URLs
+var urls = $('.rg_di .rg_meta').map(function() {
+    return JSON.parse($(this).text()).ou;
+});
+
+// write the URLs to file
+var textToSave = urls.toArray().join('\n');
+var hiddenElement = document.createElement('a');
+hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave);
+hiddenElement.target = '_blank';
+hiddenElement.download = 'urls.txt';
+hiddenElement.click();
